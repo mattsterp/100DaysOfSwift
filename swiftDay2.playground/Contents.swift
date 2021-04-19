b622e14b61677896f68c0c7616234db0a07b2b43
Matthhew Pters <mattsterp@gmail.com>
2021-04-19 14:51:45 -0400

    enums

--- swiftDay2.playground/Contents.swift
+++ swiftDay2.playground/Contents.swift
@@ -121,5 +121,77 @@ var results2 = Array<Int>()
 // For example, if you wanted to write some code to represent the success or failure of some work you were doing, you could represent that as strings:
 let result = "failure"
 // But what happens if someone accidentally uses different naming?
+let result2 = "failed"
+let result3 = "fail"
 
- 
+// All those three are different strings, so they mean different things.
+
+// With enums we can define a Result type that can be either success or failure, like this:
+enum Result {
+    case success
+    case failure
+}
+
+// And now when we use it we must choose one of those two values:
+let result4 = Result.failure
+// This stops you from accidentally using different strings each time.
+
+// Enum associated values
+// As well as storing a simple value, enums can also store associated values attached to each case. This lets you attach additional information to your enums so they can represent more nuanced data.
+
+// For example, we might define an enum that stores various kinds of activities:
+//enum Activity {
+//    case bored
+//    case running
+//    case talking
+//    case singing
+//}
+
+// That lets us say that someone is talking, but we don’t know what they are talking about, or we can know that someone is running, but we don’t know where they are running to.
+
+// Enum associated values let us add those additional details:
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+    
+}
+
+// Now we can be more precise – we can say that someone is talking about football:
+
+let talking = Activity.talking(topic: "football")
+
+// Enums raw values
+// Sometimes you need to be able to assign values to enums so they have meaning. This lets you create them dynamically, and also use them in different ways.
+
+// For example, you might create a Planet enum that stores integer values for each of its cases:
+//enum Planet: Int {
+//    case mercury
+//    case venus
+//    case earth
+//    case mars
+//}
+
+// Swift will automatically assign each of those a number starting from 0, and you can use that number to create an instance of the appropriate enum case. For example, earth will be given the number 2, so you can write this:
+
+let earth = Planet(rawValue: 3)
+
+// If you want, you can assign one or more cases a specific value, and Swift will generate the rest. It’s not very natural for us to think of Earth as the second planet, so you could write this:
+enum Planet: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
+// Now Swift will assign 1 to mercury and count from there, meaning that earth is now the third planet.
+
+// Summary
+//Arrays, sets, tuples, and dictionaries let you store a group of items under a single value. They each do this in different ways, so which you use depends on the behavior you want.
+//Arrays store items in the order you add them, and you access them using numerical positions.
+//Sets store items without any order, so you can’t access them using numerical positions.
+//Tuples are fixed in size, and you can attach names to each of their items. You can read items using numerical positions or using your names.
+//Dictionaries store items according to a key, and you can read items using those keys.
+//Enums are a way of grouping related values so you can use them without spelling mistakes.
+//You can attach raw values to enums so they can be created from integers or strings, or you can add associated values to store additional information about each case.
